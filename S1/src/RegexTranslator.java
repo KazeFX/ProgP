1abd237d75b5cf5b4e077b1ddf715a2d37887544
Peter Miscevic <peter.miscevic@gmail.com>
2014-05-21 23:05:21 +0200

    main file changed

---
+++ S1/src/RegexTranslator.java
@@ -0,0 +1,5 @@
+/**
+ * Created by M4Sh on 2014-05-21.
+ */
+public class RegexTranslator {
+}
--- S1/src/main.java
+++ S1/src/main.java
@@ -1,9 +1,14 @@
+import javax.swing.*;
+
 /**
  * Created by M4Sh on 2014-05-21.
  */
-public class main {
+
+public class Main {
+    RegexTranslator regex = new RegexTranslator();
 
     public static void main(String[] args) {
-
+        JOptionPane.showInputDialog("ENTER THE FRAY)");
+        JOptionPane.showMessageDialog(null," ");
     }
 }
