a7ff9fa777d50bba8dd7f904316e146af8f62cb1
Peter Miscevic <peter.miscevic@gmail.com>
2014-05-21 23:40:33 +0200

    Added regex and scanner.

--- S1/src/RegexTranslator.java
+++ S1/src/RegexTranslator.java
@@ -1,5 +1,31 @@
 /**
  * Created by M4Sh on 2014-05-21.
  */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class RegexTranslator {
+    Pattern regex;
+    Scanner inFile;
+
+    public RegexTranslator(File textToTranslate){
+        this.regex.pattern();
+        try {
+            this.inFile = new Scanner(textToTranslate);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File doTheMagic() {
+        while (this.inFile.hasNextLine() == true) {
+            regex.matcher(inFile.nextLine());
+
+        }
+        return null;
+    }
+
 }
